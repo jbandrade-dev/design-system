17402eef3d4bffd3a6ee344d083a68548ff68c6b
joaoandradefilho <joaoandradefilho>
2022-10-17 23:00:28 -0300

    images

--- src/components/novo3.tsx
+++
@@ -1,8 +0,0 @@
-export function Novo3() {
-  <div>
-    <video className="w-full max-w-full h-auto" controls={true}>
-      <source src="/docs/videos/flowbite.mp4" type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
-  </div>;
-}
